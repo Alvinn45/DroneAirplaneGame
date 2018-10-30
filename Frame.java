2e1e105c0a5583512db97595f73302bca6b883ac
DatDarren <42588051+DatDarren@users.noreply.github.com>
2018-10-30 10:29:19 -0700

    Add files via upload
    Here is the frame class, a simple tester, and png files

---
+++ Frame.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Frame extends JFrame{
+	
+	public Frame(){
+		
+	}
+	
+	public void create(){
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel(new BorderLayout());
+		JPanel bottom = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		JPanel top = new JPanel();
+		
+		JButton up = new JButton("Up");
+		JButton down = new JButton("Down");
+		
+		bottom.add(up);
+		bottom.add(down);
+		
+		panel.add(top, BorderLayout.NORTH);
+		panel.add(bottom, BorderLayout.SOUTH);
+		
+		JLabel label = new JLabel("Start");
+		top.add(label);
+		
+		frame.setSize(700, 700);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+}
---
+++ Tester.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Tester {
+
+	public static void main(String[] args){
+		Frame frame = new Frame();
+		frame.create();
+		 
+	}
+}
