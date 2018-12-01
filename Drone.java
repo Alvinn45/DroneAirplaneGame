3b4112c8d1cdc0f09e8d71b5283f599aadb92787
Alvinn45 <alvin.a.nguyen@sjsu.edu>
2018-12-01 15:36:32 -0800

    Lives for Drone

--- Drone.java
+++ Drone.java
@@ -9,7 +9,39 @@ import javax.swing.*;
  
 public class Drone extends Aircraft {
 	
+	private int lives;
+
 	public Drone(int x, int y, String imgFile) {
 		super(x, y, imgFile);
+	}
+
+	/**
+	 * Set the number of lives for the drone.
+	 * @param lives
+	 */
+	public void setLives(int lives) {
+		this.lives = lives;
+	}
+
+	/**
+	 * Retrieve number of lives from the drone.
+	 * @return lives
+	 */
+	public int getLives() {
+		return lives;
+	}
+
+	/**
+	 * Add a life point to the drone.
+	 */
+	public void addLife() {
+		lives++;
+	}
+
+	/**
+	 * Subtract a life point from the drone.
+	 */
+	public void subtractLife() {
+		lives--;
 	}
 }
