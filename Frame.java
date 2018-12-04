2e6ac408b5d1a1afa244494a4468022ef9272c1a
DatDarren <42588051+DatDarren@users.noreply.github.com>
2018-12-04 13:45:13 -0800

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -10,14 +10,7 @@ public class Frame extends JFrame{
 		JPanel bottom = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		JPanel top = new JPanel();
 		
-		JButton up = new JButton("Up");
-		JButton down = new JButton("Down");
-		
-		bottom.add(up);
-		bottom.add(down);
-		
 		panel.add(top, BorderLayout.NORTH);
-		panel.add(bottom, BorderLayout.SOUTH);
 		
 		JLabel label = new JLabel("Start");
 		top.add(label);
