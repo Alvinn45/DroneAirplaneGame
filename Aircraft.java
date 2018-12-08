4a803370394ca4a3b6e83f19536499ec7e24e12d
Spencer Enriquez <44066348+spencer-enriquez@users.noreply.github.com>
2018-12-07 16:32:01 -0800

    Needed to reset Bounds on its Hitbox

--- Aircraft.java
+++ Aircraft.java
@@ -1,10 +1,12 @@
 /**
  * Aircraft created by flying object.
  * @author Alvin Nguyen
+ * @author Spencer Enriquez
  */
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
+import java.util.*;
 import javax.imageio.*;
 import javax.swing.*;
 
@@ -15,26 +17,31 @@ public abstract class Aircraft implements Icon {
 	private int w;
 	private int h;
 	private int angle;
-	private String imgFile;
 	private BufferedImage img;
+	Hitbox zone;
 	
 	public Aircraft(int x, int y, String imgFile) {
 		this.x = x;
 		this.y = y;
-		this.imgFile = imgFile;
 		try {
 			img = ImageIO.read(new File(imgFile));
 			w = img.getWidth();
 			h = img.getHeight();
 			angle = (w == h) ? 90 : 0;
+			zone = new Hitbox(this);			
 		} catch (IOException io) {
 			System.out.println(io);
 		}
+	}
+	
+	public Hitbox getHitbox() {
+		return zone;
 	}
 
 	public void setLocation(int x, int y) {
 		this.x += x;
 		this.y += y;
+		this.getHitbox().resetBounds();
 	}	
 
 	public int getX() {
@@ -43,6 +50,7 @@ public abstract class Aircraft implements Icon {
 
 	public void setX(int x) {
 		this.x = x;
+		this.getHitbox().resetBounds();
 	}
 
 	public int getY() {
@@ -51,6 +59,7 @@ public abstract class Aircraft implements Icon {
 
 	public void setY(int y) {
 		this.y = y;
+		this.getHitbox().resetBounds();
 	}
 
 	public int getIconWidth() {  
@@ -66,11 +75,5 @@ public abstract class Aircraft implements Icon {
 		if (w == h) g2.rotate(Math.toRadians(angle), w / 2, h / 2);
 		g2.drawImage(img, x, y, null);
 	}
-
-	/**
-	 * Return aircraft type.
-	 * @return aircraft
-	 */
-	public abstract String toString();
 
 }
